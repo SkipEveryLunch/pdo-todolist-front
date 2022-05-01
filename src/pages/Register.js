4e30d86d743b2120a9badf0356b8f0af36a5b437
skipeverylunch <skipeverylunch@outlook.jp>
2022-05-01 18:35:32 +0900

    created register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Register = () => {
-  return <section class="bg-red-200"></section>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'email':
+        setEmail(e.target.value);
+        break;
+      case 'password':
+        setPassword(e.target.value);
+        break;
+      default:
+        console.log('key not found');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section class="grid place-items-center">
+      <form
+        className="flex flex-col gap-7 w-1/3 items-start"
+        onSubmit={handleSubmit}
+      >
+        <div className="w-full border-b border-gray-200 flex">
+          <label htmlFor="name">name:</label>
+          <input
+            className="flex-auto px-2 py-1 focus:outline-none"
+            id="name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-full border-b border-gray-200 flex">
+          <label htmlFor="email">email:</label>
+          <input
+            className="flex-auto px-2 py-1 focus:outline-none"
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-full border-b border-gray-200 flex">
+          <label htmlFor="password">password:</label>
+          <input
+            className="flex-auto px-2 py-1 focus:outline-none"
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-full flex justify-end">
+          <button className="bg-gray-200 px-2 py-1 rounded-sm">Register</button>
+        </div>
+      </form>
+    </section>
+  );
 };
 export default Register;
