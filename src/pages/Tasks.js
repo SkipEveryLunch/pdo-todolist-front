ad2cfe2277bc9b86acad8b205b0ed50cdc9e900f
skipeverylunch <skipeverylunch@outlook.jp>
2022-05-02 23:19:15 +0900

    implemented basic tasks page

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import client from '../client';
 const Tasks = () => {
-  return <div>Here goes the tasks!</div>;
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const fetchTasks = async () => {
+      let result;
+      try {
+        result = await client.get('tasks', {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+          },
+        });
+        setTasks(result.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchTasks();
+  }, []);
+  return (
+    <section className="flex flex-col">
+      <ul>
+        {tasks.map((task) => {
+          return <li>{task.name}</li>;
+        })}
+      </ul>
+    </section>
+  );
 };
 export default Tasks;
