55c00c424cca299fe8c0d2a306c63789caeebb46
skipeverylunch <skipeverylunch@outlook.jp>
2022-05-03 11:31:43 +0900

    moved Inputs into a dedicated file

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Input from '../components/Input';
 import client from '../client';
 const Register = () => {
   const [name, setName] = useState('');
@@ -40,39 +41,9 @@ const Register = () => {
         className="flex flex-col gap-7 w-1/3 items-start"
         onSubmit={handleSubmit}
       >
-        <div className="w-full border-b border-gray-200 flex">
-          <label htmlFor="name">name:</label>
-          <input
-            className="flex-auto px-2 py-1 focus:outline-none"
-            id="name"
-            name="name"
-            type="text"
-            value={name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="w-full border-b border-gray-200 flex">
-          <label htmlFor="email">email:</label>
-          <input
-            className="flex-auto px-2 py-1 focus:outline-none"
-            id="email"
-            name="email"
-            type="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="w-full border-b border-gray-200 flex">
-          <label htmlFor="password">password:</label>
-          <input
-            className="flex-auto px-2 py-1 focus:outline-none"
-            id="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-        </div>
+        <Input name="name" value={name} handleChange={handleChange} />
+        <Input name="email" value={email} handleChange={handleChange} />
+        <Input name="password" value={password} handleChange={handleChange} />
         <div className="w-full flex justify-end">
           <button className="bg-gray-200 px-2 py-1 rounded-sm">Register</button>
         </div>
